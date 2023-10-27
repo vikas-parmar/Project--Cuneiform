2584cade689d6578db1d08c466b966ba06bbe5eb
Vikas Parmar <vkparmar003@gmail.com>
2023-10-27 10:55:36 +0530

    Made index.ts to exports images & added 2 more img

---
+++ src/assets/index.ts
@@ -0,0 +1,21 @@
+import hero_img from "./hero.png";
+import write from "./write.png";
+import line from "./line.png";
+import brand_1 from "./brand-1.png";
+import brand_2 from "./brand-2.png";
+import brand_3 from "./brand-3.png";
+import brand_4 from "./brand-4.png";
+import brand_5 from "./brand-5.png";
+import brand_6 from "./brand-6.png";
+
+
+const brands = [
+    brand_1,
+    brand_2,
+    brand_3,
+    brand_4,
+    brand_5,
+    brand_6,
+]
+
+export { hero_img, write, line, brands }
